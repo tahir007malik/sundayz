3f074b26a742f6adb2ed11105a540afe027b58a9
tahir7malik <tahir.malik296@gmail.com>
2024-12-02 15:01:30 +0530

    updated config.py by removing hardcoded credentials for your database, created .env file

--- Backend/Database/config.py
+++ Backend/Database/config.py
@@ -1,9 +1,12 @@
 # Backend/Database/config.py
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "my_key")
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "1482617"
-    MYSQL_DB = "sundayz"
-    MYSQL_HOST = "localhost"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
