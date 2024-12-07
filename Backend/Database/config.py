cc4cb5bd944040d26b5d2d4a4d25e656c0c63f24
tahir7malik <tahir.malik296@gmail.com>
2024-12-07 22:46:52 +0530

    1st Phase Completed - Menu Management

--- Backend/Database/config.py
+++ Backend/Database/config.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
