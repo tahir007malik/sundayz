da70c7d6f3e80e472653429977b8894b13c93f18
tahir007malik <tahir.malik296@gmail.com>
2025-04-03 17:26:36 +0530

    added frontend and completed the registerUser for frontend

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,12 @@
 # Backend/app.py
 from flask import Flask
+from flask_cors import CORS
 from Database.config import Config
 from Database.db import mysql, init_db
 from Routes import init_app as init_routes # Import the route initialization function
 
 app = Flask(__name__)
+CORS(app) # Cross-Origin Resource Sharing between Brower(React) and Server(Flask)
 app.config.from_object(Config) # Load the configuration settings (including DB credentials)
 
 # Initialize the database connection
@@ -14,4 +16,4 @@ init_db(app)
 init_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = 8000)
